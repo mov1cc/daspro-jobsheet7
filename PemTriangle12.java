95294de087cac1ba3ab82065ee7688828708faf9
mov1cc <imovic004@gmail.com>
2025-10-29 13:38:04 +0700

    Pembenaran Triangle 12

---
+++ PemTriangle12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PemTriangle12 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        
+        int i = 1; 
+        
+        while (i <= n) { 
+            int j = 1;   
+            while (j <= i) { 
+                System.out.print("*");
+                j++;
+            }
+            System.out.println(); 
+            i++;
+        }
+        
+        sc.close();
+    }
+}
+
